c35a340247eabba5561c8aa23b1822a0d7569ce6
abdul khaliq arrachman <59369047+abdulkhaliqumb@users.noreply.github.com>
2024-05-16 11:59:14 +0700

    Add files via upload
    revisi 16/05/24 jam 12:00

--- tugas11.js
+++ tugas11.js
@@ -6,40 +6,33 @@ const data = JSON.parse(result);
 let next=0;
 let winner=0;
 let maxQuiz=data.length-1;
-let rl;
 
-rl = readline.createInterface({
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  prompt: `Pertanyaan : data ${data[next].definition} \nJawaban : `, 
+  prompt: `Tebakan : `, 
 });
-
+console.log( `Pertanyaan : data ${data[next].definition} `)
 rl.prompt();  
 
-rl.on("line", (line) => {  
+rl.on("line", (line) => {   
     if (data[next].term.toUpperCase().trim()==line.toUpperCase().trim()){
         winner++;
         console.log('Selamat Anda benar !');
     }  else{
         console.log('Wwkkk, Anda belum beruntung !');
     }
-
  
     if (next == maxQuiz ){
       
         if (winner==(maxQuiz+1)) {
             console.log('Hore Anda Menang!');
          }
-        process.exit(0);
+    process.exit(0);
     }
     next++
  
-    rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-        prompt: `Pertanyaan : data ${data[next].definition} \nJawaban : `, 
-    });
-   
+    console.log( `Pertanyaan : data ${data[next].definition} `)
     rl.prompt(); 
 }).on("close", (line) => {
     process.exit(0);
